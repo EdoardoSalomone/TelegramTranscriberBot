f0d1aa3be0c72226bb76e239ecb7af50294c7684
edoardo <salomone.edoardo@gmail.com>
2025-05-06 13:48:12 +0200

    adding log for env variables

--- src/main/java/it/transcriberbot/transcriberbot/TranscriberBotApplication.java
+++ src/main/java/it/transcriberbot/transcriberbot/TranscriberBotApplication.java
@@ -1,6 +1,10 @@
 package it.transcriberbot.transcriberbot;
 
 import it.transcriberbot.transcriberbot.bot.TelegramBot;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +14,18 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
+@Slf4j
 public class TranscriberBotApplication {
+
+
+    @PostConstruct
+    public void logEnvVars() {
+        String token = System.getenv("TELEGRAM_BOT_TOKEN");
+        String username = System.getenv("TELEGRAM_BOT_USERNAME");
+
+        log.info("bot token : " + token);
+        log.info("bot username : " + username);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TranscriberBotApplication.class, args);
